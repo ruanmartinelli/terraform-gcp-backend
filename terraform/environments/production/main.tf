859413851d652ec902c107ae9b068f13707c2f6f
Ruan <martinelliruan@gmail.com>
2020-11-02 22:44:12 +0100

    Initial commit

---
+++ terraform/environments/production/main.tf
@@ -0,0 +1,30 @@
+locals {
+  env = "production"
+
+  # Add the project id to `terraform.tfstate`
+  project = var.project
+
+  region = "us-central1"
+  location = "us-central"
+  
+  function_name="api-production"
+  function_entry_point="app"
+}
+
+provider "google" {
+  project = local.project
+  region  = local.region
+}
+
+module "function" {
+  source      = "../../modules/function"
+  project     = local.project
+  name        = local.function_name
+  entry_point = local.function_entry_point
+}
+
+module "database" {
+  source  = "../../modules/database"
+  project = local.project
+  location  = local.location
+}
---
+++ terraform/modules/database/main.tf
@@ -0,0 +1,15 @@
+# Enable Firestore API
+resource "google_project_service" "db" {
+  project = var.project
+  service = "firestore.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
+
+# Create Firestore database
+resource "google_app_engine_application" "app" {
+  project       = var.project
+  location_id   = var.location
+  database_type = "CLOUD_FIRESTORE"
+}
